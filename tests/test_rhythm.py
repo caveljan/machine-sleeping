4ec67a048663a76ec86ce8f45f444681491d2411
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-27 22:41:12 +0200

    test: rhythm

--- tests/test_rhythm.py
+++ tests/test_rhythm.py
@@ -6,7 +6,7 @@ from machine_sleeping.entity import CircamentRhythm
 
 
 class TestCircamentRhythm(unittest.TestCase):
-   def test_circadian_rhythm(self):
+   def test_circament_rhythm(self):
         rhythm = CircamentRhythm('sleep', 1, 1)
         rhythm.setState('awake', 1, 0.3)
 
