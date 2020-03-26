46024d58fbadb93675c8eeaa2f2e78526e19f85d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-26 23:38:42 +0200

    dev: dream

---
+++ machine_sleeping/dream.py
@@ -0,0 +1,16 @@
+"""
+What is to dream?
+A dance of retracing and detracing the steps taken while awake (the life).
+"""
+class Dream:
+    def __init__(self, life):
+        self.life = life
+        pass
+
+    def retracing(self):
+        # retake the self.step
+        pass
+
+    def detracing(self):
+        # detake the self.step
+        pass
